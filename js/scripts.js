839a2e9a06562eb9f4e76381ba9c2215f12c0949
AndySkaura <andy_skayra@sina.com>
2019-12-02 13:38:58 +0800

    more

--- js/scripts.js
+++ js/scripts.js
@@ -175,9 +175,9 @@
 		},
 	});
 	function getUrlParam(param) {
-		var reg = new RegExp("(^|&)" + param + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
-		var r = window.location.search.substr(1).match(reg);  //匹配目标参数
-		if (r != null) return decodeURI(r[2]); return null; //返回参数值
+		var reg = new RegExp("(^|&)" + param + "=([^&]*)(&|$)"); 
+		var r = window.location.search.substr(1).match(reg); 
+		if (r != null) return decodeURI(r[2]); return null; 
 	}
 	function goToPos(){
 		$("html, body").animate({
